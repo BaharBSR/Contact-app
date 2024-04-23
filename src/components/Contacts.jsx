b4bd05e0940b916e3f0664ff27604564c23c7e1a
Bahar <basiribahare1@gmail.com>
2024-04-23 19:19:23 +0330

    contact app

---
+++ src/components/Contacts.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+
+//components
+import ContactsList from "./ContactsList";
+
+// contants
+import inputs from "../constants/inputs";
+
+//id
+import { v4 } from "uuid";
+
+// styles 
+import styles from "./Contacts.module.css"
+
+const Contacts = () => {
+  const [contact, setContact] = useState({
+    id: "",
+    name: "",
+    lastName: "",
+    email: "",
+    phone: "",
+  });
+
+  const [contacts, setContacts] = useState([]);
+  const [alert, setAlert] = useState("");
+  const changeHandler = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setContact((contact) => ({ ...contact, [name]: value }));
+  };
+
+  const addHandler = () => {
+    if (
+      !contact.name ||
+      !contact.lastName ||
+      !contact.email ||
+      !contact.phone
+    ) {
+      setAlert("please enter valid data!");
+      return;
+    }
+
+    setAlert("");
+
+    const newContact = { ...contact, id: v4() };
+
+    setContacts((contacts) => [...contacts, newContact]);
+    setContact({
+      name: "",
+      lastName: "",
+      email: "",
+      phone: "",
+    });
+    console.log(contacts);
+  };
+
+  const deleteHandler = (id) => {
+    const newContacts = contacts.filter((contact) => contact.id !== id);
+    setContacts(newContacts);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.form}>
+        {inputs.map((input, index) => (
+          <input
+            key={index}
+            type={input.type}
+            placeholder={input.placeholder}
+            name={input.name}
+            value={contact[input.name]}
+            onChange={changeHandler}
+          />
+        ))}
+
+        <button onClick={addHandler}>Add Contact</button>
+      </div>
+      <div className={styles.alert}>{alert && <p>{alert}</p>}</div>
+      <ContactsList contacts={contacts} deleteHandler={deleteHandler} />
+    </div>
+  );
+};
+
+export default Contacts;
